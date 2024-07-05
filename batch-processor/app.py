c71e4d4470b278550e6dfd9a2b8163fe3939362f
Karolina Wieteska <wieteskakarolina01@gmail.com>
2024-07-05 10:14:55 +0200

    Add batch processor method.

--- batch-processor/app.py
+++ batch-processor/app.py
@@ -8,3 +8,30 @@ class BatchProcessor:
 
     def _size_of_record(self, record):
         return len(record.encode('utf-8'))
+    
+    def process_batches(self):
+        batches = []
+        current_batch = []
+        current_batch_size = 0
+        current_record_count = 0
+
+        for record in self.records:
+            record_size = self._size_of_record(record)
+
+            if record_size > self.MAX_RECORD_SIZE:
+                continue
+
+            if (current_batch_size + record_size <= self.MAX_BATCH_SIZE) and (current_record_count < self.MAX_RECORD_COUNT):
+                current_batch.append(record)
+                current_batch_size += record_size
+                current_record_count += 1
+            else:
+                batches.append(current_batch)
+                current_batch = [record]
+                current_batch_size = record_size
+                current_record_count = 1
+
+        if current_batch:
+            batches.append(current_batch)
+
+        return batches
